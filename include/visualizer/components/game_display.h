9a7094c11a5ac9fb11fb1d85e0a1e40856538b17
Thailer Lietz <thailer.lietz@yahoo.com>
2020-11-28 20:36:11 -0600

    Added documentation to GameDisplay.

--- include/visualizer/components/game_display.h
+++ include/visualizer/components/game_display.h
@@ -8,17 +8,20 @@ namespace game {
 
 namespace visualizer {
 
+// Represents a GUI component that displays a game engine's contents.
 class GameDisplay : public Component {
  public:
+  // Creates a display with game engine, position, and size.
   GameDisplay(const GameEngine& game_engine,
               const glm::vec2& top_left_corner = {},
               const glm::vec2& dimensions = {});
-
+  // Draws the game.
   void Draw() const override;
 
  private:
   const GameEngine& game_engine_;
 
+  // Draws one AABB to the window.
   void DrawAABB(AABB box) const;
 };
 
